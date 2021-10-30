86c656ffe3d216eee74cd96ab7d527a99f771502
Chisama Musonda <Chase.Musonda@gmail.com>
2021-10-30 18:55:07 +0200

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=udagram.cltlhgqhhbco.af-south-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::chase-udagram-dev
+export AWS_REGION=af-south-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
