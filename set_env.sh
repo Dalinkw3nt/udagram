3eae1fee2783588a46645c6b85e64fe421c22cde
Chisama Musonda <Chase.Musonda@gmail.com>
2021-10-30 18:56:32 +0200

    removing credentials

--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,7 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=postgres
+export POSTGRES_USERNAME=
+export POSTGRES_PASSWORD=
 export POSTGRES_HOST=udagram.cltlhgqhhbco.af-south-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::chase-udagram-dev
